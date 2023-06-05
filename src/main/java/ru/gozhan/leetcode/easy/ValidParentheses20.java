1ecaf1af8e0fc7b28b51f406f5568e83405a765a
Sanchellik <sashagozhan@gmail.com>
2023-06-05 19:01:45 +0300

    #20 done

---
+++ src/main/java/ru/gozhan/leetcode/easy/ValidParentheses20.java
@@ -0,0 +1,41 @@
+package ru.gozhan.leetcode.easy;
+
+import java.util.Stack;
+
+/**
+ * Link to task #20 <a href="https://leetcode.com/problems/longest-common-prefix/">...</a>
+ *
+ */
+
+public class ValidParentheses20 {
+
+    public static void main(String[] args) {
+
+        System.out.println(Solution.isValid("()[]{}"));
+
+    }
+
+    static class Solution {
+
+        public static boolean isValid(String s) {
+            Stack<Character> stack = new Stack<>();
+            for (char ch : s.toCharArray()) {
+                switch (ch) {
+                    case '(', '[', '{' -> stack.push(ch);
+                    case ')' -> {
+                        if (stack.empty() || stack.pop() != '(') return false;
+                    }
+                    case ']' -> {
+                        if (stack.empty() || stack.pop() != '[') return false;
+                    }
+                    case '}' -> {
+                        if (stack.empty() || stack.pop() != '{') return false;
+                    }
+                }
+            }
+            return stack.empty();
+        }
+
+    }
+
+}
